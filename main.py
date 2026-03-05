5ba157855d9270ff0d6d4da74f9303a8305b1e5b
Vikas <vlohana103@gmail.com>
2026-03-05 04:19:31 -0500

    removed hard code so user can ask in command line

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -13,8 +14,18 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+
+    usage = response.usage_metadata    
+
+    if usage == None:
+        raise RuntimeError("usage is None")
+
+    print(f"Prompt tokens: {usage.prompt_token_count}")
+    print(f"Response tokens: {usage.candidates_token_count}")
     print(response.text)
 
 if __name__ == "__main__":
