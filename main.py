bba6249cc640f8dfbc2b40ce6af98ecff8720b77
Vikas <vlohana103@gmail.com>
2026-03-05 02:52:08 -0500

    first push, and made sure API key is working

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    print("Hello from ai-agent!")
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key == None:
+        raise RuntimeError("API Key is None")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
