010a41566dbcb4af00790f9ade95683214d17676
Vikas <vlohana103@gmail.com>
2026-03-05 04:44:49 -0500

    added --verbose to make answers more detailed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     print("Hello from ai-agent!")
@@ -16,17 +17,25 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
-    args = parser.parse_args()
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+
+
+    args = parser.parse_args()    
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
 
     usage = response.usage_metadata    
 
     if usage == None:
         raise RuntimeError("usage is None")
 
-    print(f"Prompt tokens: {usage.prompt_token_count}")
-    print(f"Response tokens: {usage.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {usage.prompt_token_count}")
+        print(f"Response tokens: {usage.candidates_token_count}")
     print(response.text)
 
 if __name__ == "__main__":
     main()
+
+
